ac9598c114c646586ec333edd90aad9a7b36a4fb
POR7ALa <thatsair@gmail.com>
2024-10-01 20:42:39 +1300

    chore: add bulk migration/seed files

---
+++ server/db/seed/all_seeds.sql
@@ -0,0 +1,9 @@
+-- Read all the seed files in sequence
+.read ./seed/001_seed_users_data.sql
+.read ./seed/002_seed_schedule_data.sql
+.read ./seed/003_seed_courses_data.sql
+.read ./seed/004_seed_tasks_data.sql
+.read ./seed/005_seed_projects_data.sql
+.read ./seed/006_seed_technologies_data.sql
+.read ./seed/007_seed_todos_data.sql
+.read ./seed/008_seed_notes_data.sql
