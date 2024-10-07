6dbbf8c045ef1d76740d28365d54b1059bc523bd
POR7ALa <thatsair@gmail.com>
2024-10-07 20:48:07 +1300

    update: relative file location for db location context

--- server/migrate/all_migrations.up.sql
+++ server/migrate/all_migrations.up.sql
@@ -1,9 +1,9 @@
 -- Read all the up migration files in sequence
-.read ./migrate/001_create_users_table.up.sql
-.read ./migrate/002_create_schedule_table.up.sql
-.read ./migrate/003_create_courses_table.up.sql
-.read ./migrate/004_create_tasks_table.up.sql
-.read ./migrate/005_create_projects_table.up.sql
-.read ./migrate/006_create_technologies_table.up.sql
-.read ./migrate/007_create_todos_table.up.sql
-.read ./migrate/008_create_notes_table.up.sql
+.read ../migrate/001_create_users_table.up.sql
+.read ../migrate/002_create_schedule_table.up.sql
+.read ../migrate/003_create_courses_table.up.sql
+.read ../migrate/004_create_tasks_table.up.sql
+.read ../migrate/005_create_projects_table.up.sql
+.read ../migrate/006_create_technologies_table.up.sql
+.read ../migrate/007_create_todos_table.up.sql
+.read ../migrate/008_create_notes_table.up.sql
