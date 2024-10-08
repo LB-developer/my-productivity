e4fe9b289b938b00b73b35caa4c258d9b7a16c51
POR7ALa <thatsair@gmail.com>
2024-10-09 12:30:08 +1300

    fix: dashboard layout to include sidebar
    - width of widgets is handled automatically
    - previous sidebar removed to be manually replaced

--- client/Pages/Dashboard/Dashboard.tsx
+++ client/Pages/Dashboard/Dashboard.tsx
@@ -6,55 +6,60 @@ import MonthlySpend from "../../components/Widgets/MonthlySpend"
 import TodaysTasks from "../../components/Widgets/TodaysTasks"
 import NewStudySession from "../../components/Widgets/NewStudySession"
 import UserOverview from "../../components/Widgets/UserOverview"
+import Sidebar from "../../components/Sidebar"
 import { Container, Row, Col } from "react-bootstrap"
 
 export default function Dashboard() {
   const userId: number = 1
 
   return (
-    <Container fluid className="bg-secondary bg-opacity-25 min-vh-100">
-      <Row className="gap-3 gy-3">
-        {/* Header Section */}
-        <Col
-          xs={10}
-          className="offset-2 d-flex flex-column justify-content-center"
-        >
-          <p className="mb-0 mt-4">Welcome to your,</p>
-          <h2 className="fw-bold display-4">Productivity Dashboard</h2>
-        </Col>
+    <div className="d-flex flex-row gap-2 bg-secondary bg-opacity-25">
+      <Sidebar />
+      <Container fluid className="bg-secondary bg-opacity-10 min-vh-100">
+        <Col className="flex-grow-1"></Col>
+        <Row className="gap-3 gy-2">
+          <Row className="gap-3 gy-2">
+            {/* Header Section */}
+            <Col xs={12} className="d-flex flex-column justify-content-center">
+              <p className="mb-0 mt-4">Welcome to your,</p>
+              <h2 className="fw-bold display-4">Productivity Dashboard</h2>
+            </Col>
+            <Col className="bg-light rounded-4">
+              <MonthlyHours userId={String(userId)} />
+            </Col>
 
-        <Col xs={3} className="offset-2 bg-light rounded-4">
-          <MonthlyHours userId={String(userId)} />
-        </Col>
+            <Col className="bg-danger bg-opacity-50">
+              <MonthlySpend />
+            </Col>
 
-        <Col xs={3} className="bg-danger bg-opacity-50">
-          <MonthlySpend />
-        </Col>
+            <Col className="bg-light rounded-4">
+              <NewStudySession />
+            </Col>
+          </Row>
+          <Row className="gap-3 gy-2">
+            <Col className="bg-light rounded-4">
+              <TodaysTasks userId={String(userId)} />
+            </Col>
 
-        <Col xs={3} className="bg-primary bg-opacity-25">
-          <NewStudySession />
-        </Col>
+            <Col className="bg-light rounded-4">
+              <CoursesPreview userId={String(userId)} />
+            </Col>
 
-        <Col xs={3} className="offset-2 bg-light rounded-4">
-          <TodaysTasks userId={String(userId)} />
-        </Col>
+            <Col className="bg-warning bg-opacity-50">
+              <ActivityPreview />
+            </Col>
+          </Row>
+          <Row className="">
+            <Col xs={6} className="bg-primary bg-opacity-50">
+              <CurrentProgress />
+            </Col>
 
-        <Col xs={3} className="bg-light rounded-4">
-          <CoursesPreview userId={String(userId)} />
-        </Col>
-
-        <Col xs={3} className="bg-warning bg-opacity-50">
-          <ActivityPreview />
-        </Col>
-
-        <Col xs={6} className="offset-2 bg-primary bg-opacity-50">
-          <CurrentProgress />
-        </Col>
-
-        <Col xs={3} className="bg-warning bg-opacity-75">
-          <UserOverview userId={userId} />
-        </Col>
-      </Row>
-    </Container>
+            <Col xs={6} className="bg-warning bg-opacity-75">
+              <UserOverview userId={userId} />
+            </Col>
+          </Row>
+        </Row>
+      </Container>
+    </div>
   )
 }
