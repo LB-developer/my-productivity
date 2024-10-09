ec1736a4d505975eb5912211a1e5dbf2c800e511
POR7ALa <thatsair@gmail.com>
2024-10-09 13:24:13 +1300

    chore: add comments to explain stopwatch logic

--- client/components/Stopwatch.tsx
+++ client/components/Stopwatch.tsx
@@ -13,12 +13,21 @@ export default function Stopwatch({
   setStudySessionTimeElapsed,
   isRunning,
 }: Props) {
+  /*
+    Our useEffect adds 1 second/minute/hour based on the current time elapsed
+
+    It works by incrementing time based on the current value  being less than 59
+    else when
+    60 seconds have passed - reset seconds to 0 and add one to minute
+    60 minutes have passed - reset minutes to 0 and add one to hour
+  */
   useEffect(() => {
     if (isRunning) {
       const interval = setInterval(() => {
         if ((studySessionTimeElapsed.get("seconds") ?? 0) < 59) {
           setStudySessionTimeElapsed((prevTime) => {
             const lastTime = new Map([...prevTime])
+            // current seconds are less than 59 e.g. XX:XX:58 therefore it is safe to increment
             lastTime.set("seconds", (lastTime.get("seconds") ?? 0) + 1)
 
             return lastTime
@@ -26,17 +35,17 @@ export default function Stopwatch({
         } else if ((studySessionTimeElapsed.get("minutes") ?? 0) < 59) {
           setStudySessionTimeElapsed((prevTime) => {
             const lastTime = new Map([...prevTime])
-            lastTime.set("minutes", (lastTime.get("minutes") ?? 0) + 1)
-            lastTime.set("seconds", 0)
+            lastTime.set("minutes", (lastTime.get("minutes") ?? 0) + 1) // increment minutes passed by one
+            lastTime.set("seconds", 0) // 60 seconds have passed - reset seconds to zero
 
             return lastTime
           })
         } else {
           setStudySessionTimeElapsed((prevTime) => {
             const lastTime = new Map([...prevTime])
-            lastTime.set("hours", (lastTime.get("hours") ?? 0) + 1)
-            lastTime.set("minutes", 0)
-            lastTime.set("seconds", 0)
+            lastTime.set("hours", (lastTime.get("hours") ?? 0) + 1) // increment hours passed by one
+            lastTime.set("minutes", 0) // 60 minutes have passed - reset minutes to zero
+            lastTime.set("seconds", 0) // 60 seconds have passed - reset seconds to zero
 
             return lastTime
           })
