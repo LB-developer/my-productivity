d3b91f7d1daf78681b7d5d2b26ac2055f8fdf12b
POR7ALa <thatsair@gmail.com>
2024-10-11 20:37:26 +1300

    feat: add focus goal component
    for users to see set daily/weekly goals and view progress/milestones

---
+++ client/components/Widgets/FocusGoalsOverview.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+export default function FocusGoalsOverview() {
+  
+  // TODO: add daily/weekly goal viewer
+  // add edit focus goals
+  return (
+    <>
+      <p>Focus Goals</p>
+    </>
+  )
+}
