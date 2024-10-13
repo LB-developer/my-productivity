afd79e3599afaa03c2f43ec79b949196ab12e6d0
lblmdesk <loganbendev@gmail.com>
2024-10-13 19:24:34 +1300

    chore: formatting

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+
 	"productivity/server/router"
 )
-
 
 func main() {
 	r := router.SetupRoutes()
 	fmt.Println("Starting Server 8080")
-    http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8080", r)
 }
-
