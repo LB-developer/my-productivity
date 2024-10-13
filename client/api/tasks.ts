9c35659ed1458ab7efd4bcf5a1ba307837b5b3dc
lblmdesk <loganbendev@gmail.com>
2024-10-13 18:35:43 +1300

    feat: add fetchAllTasks
    - calls the tasks api with userId as a query and receives all tasks that
    match the user id i.e., that are the users.
    
    ALSO
    - update error handling for fetchLastThirtyMinutes

--- client/api/tasks.ts
+++ client/api/tasks.ts
@@ -1,5 +1,5 @@
 import { ThirtyHistoryLineGraph } from "./models"
-import { TodaysTasksData } from "./models"
+import { TodaysTasksData, TaskData } from "./models"
 
 export async function fetchLastThirtyMinutes(
   userId: string
@@ -12,9 +12,15 @@ export async function fetchLastThirtyMinutes(
         "Content-Type": "application/json",
       },
     }
-  ).then((response) => response.json())
+  )
+  const contentType = res.headers.get("Content-Type")
+  if (!res.ok || !contentType?.includes("application/json")) {
+    const err = await res.text()
+    console.error("Error:", err)
+    throw new Error("Failed to fetch completed hours in the last 30 days. Response was not JSON.")
+  }
 
-  return res
+  return res.json()
 }
 
 export async function fetchTodaysTasks(
@@ -22,6 +28,27 @@ export async function fetchTodaysTasks(
 ): Promise<TodaysTasksData[]> {
   const res = await fetch(
     `http://localhost:8080/api/v1/tasks/preview?userId=${userId}`,
+    {
+      method: "GET",
+      headers: { "Content-type": "application/json" },
+    }
+  )
+  const contentType = res.headers.get("Content-Type")
+  if (!res.ok || !contentType?.includes("application/json")) {
+    const err = await res.text()
+    console.error("Error:", err)
+    throw new Error("Failed to fetch todays tasks. Response was not JSON.")
+  }
+
+  return res.json()
+}
+
+
+export async function fetchAllTasks(
+  userId: string
+): Promise<TaskData[][]> {
+  const res = await fetch(
+    `http://localhost:8080/api/v1/tasks?userId=${userId}`,
     {
       method: "GET",
       headers: { "Content-type": "application/json" },
