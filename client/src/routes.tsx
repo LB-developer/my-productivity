a5604018854b9aa202abd01459a597d57b22bab9
lblmdesk <loganbendev@gmail.com>
2024-10-14 20:50:20 +1300

    chore: add tasks route to react router

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,11 +2,13 @@ import { createRoutesFromElements, Route } from 'react-router-dom'
 
 import App from '../components/App'
 import Dashboard from '../Pages/Dashboard/Dashboard'
+import Tasks from '../Pages/Tasks.tsx'
 
 export const routes = createRoutesFromElements(
-  <Route path="/" element={<App />}>
-    <Route index element={<Dashboard />} />
-  </Route>
+        <Route path="/" element={<App />}>
+                <Route index element={<Dashboard />} />
+                <Route path="/tasks" element={<Tasks />} />
+        </Route>
 )
 
 export default routes
