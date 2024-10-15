fa9339dec6fbef5efd120c4e929de131f0aff748
lblmdesk <loganbendev@gmail.com>
2024-10-15 15:53:44 +1300

    feat: global sidebar
    - sidebar was previously in the dashboard, moved for simplicity of
    control and management

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom"
+import Sidebar from "./Sidebar"
 function App() {
   return (
-    <div className="">
-      <Outlet />
+    <div className="app-container d-flex flex-row">
+      <Sidebar />
+      <div className="content-area">
+        <Outlet />
+      </div>
     </div>
   )
 }
