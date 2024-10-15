7fbcc76330a5ef13434fa48c4f46e31c48b24dea
lblmdesk <loganbendev@gmail.com>
2024-10-15 15:50:23 +1300

    chore: add routes for all pages
    - final page selections are still tbd i.e., including schedule/projects
    - but as now it is more important to get the view as close to functional
    as possible to avoid compounding changes

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,13 +2,23 @@ import { createRoutesFromElements, Route } from 'react-router-dom'
 
 import App from '../components/App'
 import Dashboard from '../Pages/Dashboard/Dashboard'
+import Courses from '../Pages/Courses.tsx'
+import Activity from '../Pages/Activity.tsx'
+import Projects from '../Pages/Projects.tsx'
 import Tasks from '../Pages/Tasks.tsx'
+import Schedule from '../Pages/Schedule.tsx'
+import Settings from '../Pages/Settings.tsx'
 
 export const routes = createRoutesFromElements(
-        <Route path="/" element={<App />}>
-                <Route index element={<Dashboard />} />
-                <Route path="/tasks" element={<Tasks />} />
-        </Route>
+  <Route path="/" element={<App />}>
+    <Route index element={<Dashboard />} />
+    <Route path="/courses" element={<Courses />} />
+    <Route path="/activity" element={<Activity />} />
+    <Route path="/projects" element={<Projects />} />
+    <Route path="/tasks" element={<Tasks />} />
+    <Route path="/schedule" element={<Schedule />} />
+    <Route path="/settings" element={<Settings />} />
+  </Route>
 )
 
 export default routes
