5960b1e3d43c645cfc9ab5d4be0104a57825e07d
lblmdesk <loganbendev@gmail.com>
2024-10-17 22:15:29 +1300

    feat: filter tasks by course name

--- client/Pages/Tasks.tsx
+++ client/Pages/Tasks.tsx
@@ -1,7 +1,11 @@
-import { Table } from "react-bootstrap";
+import { Dropdown, Table } from "react-bootstrap";
 import { useGetUserTasks } from "../hooks/Tasks/Tasks"
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { TaskData } from "../api/models";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function Tasks() {
@@ -9,10 +13,40 @@ export default function Tasks() {
   // TODO: replace "1" with dynamically attained userId
   const { data: tasks, isLoading, isError, error } = useGetUserTasks("1");
 
+  // TODO: get data for names of unique courses for list
+  const courseNameList = ["Go Programming", "React Fundamentals", "View All"];
+
+  const [incompleteTaskList, setIncompleteTaskList] = useState<TaskData[]>();
+  const [taskFilter, setTaskFilter] = useState<string>("View All");
+
   const navigate = useNavigate();
 
+  // initialize incomplete task into state variable when data is fetched
+  useEffect(() => {
+    if (tasks)
+      setIncompleteTaskList(tasks[0])
+  }, [tasks])
+
+  // filter tasks by course name
+  useEffect(() => {
+    if (tasks && incompleteTaskList && taskFilter !== "View All") {
+      let currentOrder = [...tasks[1]].filter(task => task.courseName === taskFilter)
+      setIncompleteTaskList(currentOrder);
+
+    } else {
+      if (tasks) {
+        // options to filter are a list of course names or "View all"
+        // -- this is the view all path
+        setIncompleteTaskList(tasks[1])
+      }
+    }
+
+
+  }, [taskFilter])
+
   if (isLoading) {
-    return <p>Loading</p>
+    // TODO: replace with loading page, spinner, something
+    return <p>Loading Tasks...</p>
   }
 
   if (isError) {
@@ -20,9 +54,9 @@ export default function Tasks() {
     return <p>Something went wrong...</p>
   }
 
-  if (tasks) {
+  if (incompleteTaskList) {
     return (
-      <>
+      <HelmetProvider>
         <Helmet>
           <title>Tasks</title>
         </Helmet>
@@ -34,13 +68,24 @@ export default function Tasks() {
                 <th>Task Name</th>
                 <th>Task Length</th>
                 <th>Task Date</th>
-                <th>Course Name</th>
+                <th>Course Name<Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    <FontAwesomeIcon icon={faFilter} />
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    {courseNameList.map((courseName, index) =>
+                      <Dropdown.Item key={`${index}${courseName}`} onClick={() => setTaskFilter(courseName)}>{courseName}</Dropdown.Item>
+                    )}
+                  </Dropdown.Menu>
+                </Dropdown>
+                </th>
               </tr>
             </thead>
             <tbody>
-              {tasks[0].map(task =>
+              {incompleteTaskList.map(task =>
               (
-                <tr role="button" tabIndex={0} onClick={() => navigate(`/tasks/${task.taskId}`)}
+                <tr key={task.taskId} role="button" tabIndex={0} onClick={() => navigate(`/tasks/${task.taskId}`)}
                   onKeyDown={(e) => {
                     if (e.key === 'Enter' || e.key === ' ') {
                       navigate(`/tasks/${task.taskId}`);
@@ -57,7 +102,7 @@ export default function Tasks() {
             </tbody>
           </Table>
         </div>
-      </>
+      </HelmetProvider>
     )
   }
 }
