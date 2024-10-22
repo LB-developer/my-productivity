5ffe8f33f5eac47889452fc7f87eaa4a3e997ab5
lblmdesk <loganbendev@gmail.com>
2024-10-22 19:35:16 +1300

    chore: add export to config types
    references
    commit: db55ff769996

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 import 'vite/client'
 
-interface ImportMetaEnv {
+export interface ImportMetaEnv {
   readonly VITE_FIREBASE_API_KEY: string;
   readonly VITE_FIREBASE_AUTH_DOMAIN: string;
   readonly VITE_FIREBASE_PROJECT_ID: string;
@@ -10,6 +10,6 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_APP_ID: string;
 }
 
-interface ImportMeta {
+export interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
