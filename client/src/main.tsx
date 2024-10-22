edf37ae4c49e8d80ad0b934f1d7706556db23913
lblmdesk <loganbendev@gmail.com>
2024-10-22 19:33:01 +1300

    chore: wrap App in auth provider
    - allows the app to use authentication context for the user

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import routes from './routes.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import AuthProvider from './store/ContextProvider.tsx'
+
 const router = createBrowserRouter(routes)
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </StrictMode>
 )
