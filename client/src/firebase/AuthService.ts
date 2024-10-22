b7a8327a93ecfd38b99ad82bff531bd5b8ac7ca9
lblmdesk <loganbendev@gmail.com>
2024-10-22 19:29:37 +1300

    feat(incomplete): add auth service functions
    - creating a user with email and password is not working correctly (the
    submission loads infinitely)
    
    - create with google is working well - user is stored in firebase db on
    creation
    - signing out works
    
    TODO
    - add comments to the code
    - fix user creation via email/password

---
+++ client/src/firebase/AuthService.ts
@@ -0,0 +1,48 @@
+import { auth } from "./firebaseConfig.ts";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+  sendEmailVerification,
+  updatePassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+export const doCreateUserWithEmailAndPassword = async (email: string, password: string) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithEmailAndPassword = (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  const result = await signInWithPopup(auth, provider);
+  const user = result.user;
+
+  // add user to firestore
+  return result
+};
+
+export const doSignOut = () => {
+  return auth.signOut();
+};
+
+export const doPasswordReset = (email: string) => {
+  return sendPasswordResetEmail(auth, email);
+};
+
+export const doPasswordChange = (password: string) => {
+
+  if (auth.currentUser !== null)
+    return updatePassword(auth.currentUser, password);
+};
+
+export const doSendEmailVerification = () => {
+  if (auth.currentUser !== null)
+    return sendEmailVerification(auth.currentUser, {
+      url: `${window.location.origin}/home`,
+    });
+};
