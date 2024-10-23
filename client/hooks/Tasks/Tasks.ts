3033da4ab3ded57a8c98c4b4ca6d31116c2da2e8
lblmdesk <loganbendev@gmail.com>
2024-10-23 18:23:24 +1300

    feat: add hook for creating a task
    ALSO
    - change query key to work with a specific user as opposed to a generic
    'tasks'

--- client/hooks/Tasks/Tasks.ts
+++ client/hooks/Tasks/Tasks.ts
@@ -1,9 +1,10 @@
-import { useQuery } from "@tanstack/react-query"
-import { fetchLastThirtyMinutes, fetchAllTasks } from "../../api/tasks"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { fetchLastThirtyMinutes, fetchAllTasks, createNewTask } from "../../api/tasks"
+
 
 export function useGetUserTasks(userId: string) {
   return useQuery({
-    queryKey: ["tasks"],
+    queryKey: [`tasks ${userId}`],
     queryFn: () => fetchAllTasks(userId),
   })
 }
@@ -12,5 +13,15 @@ export function useGetLastThirty(userId: string) {
   return useQuery({
     queryKey: ["thirtyDayTaskHours"],
     queryFn: () => fetchLastThirtyMinutes(userId),
+  })
+}
+
+export function useCreateNewTask(userId: string) {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (userId: string) => createNewTask(userId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [`tasks ${userId}`] })
+    }
   })
 }
