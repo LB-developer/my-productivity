5eb875d852d072fbd77f10cec0fc6c7f6fc21e23
lblmdesk <loganbendev@gmail.com>
2024-10-24 22:57:17 +1300

    feat: protect specified routes
    - adds an unauthenticated user (not signed in) to only be able to see
    the homepage, login 'page', register 'page'.
    - sets the homepage as the root element of the outlet

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -8,16 +8,25 @@ import Projects from '../Pages/Projects.tsx'
 import Tasks from '../Pages/Tasks.tsx'
 import Schedule from '../Pages/Schedule.tsx'
 import Settings from '../Pages/Settings.tsx'
+import Register from '../components/firebase/Register.tsx'
+import Login from '../components/firebase/Login.tsx'
+import ProtectedRoute from '../components/ProtectedRoute.tsx'
+import HomePage from '../Pages/HomePage.tsx'
 
 export const routes = createRoutesFromElements(
   <Route path="/" element={<App />}>
-    <Route index element={<Dashboard />} />
-    <Route path="/courses" element={<Courses />} />
-    <Route path="/activity" element={<Activity />} />
-    <Route path="/projects" element={<Projects />} />
-    <Route path="/tasks" element={<Tasks />} />
-    <Route path="/schedule" element={<Schedule />} />
-    <Route path="/settings" element={<Settings />} />
+    <Route index element={<HomePage />} />
+    <Route element={<ProtectedRoute />}>
+      <Route index path="dashboard" element={<Dashboard />} />
+      <Route path="courses" element={<Courses />} />
+      <Route path="activity" element={<Activity />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="tasks" element={<Tasks />} />
+      <Route path="settings" element={<Settings />} />
+      <Route path="schedule" element={<Schedule />} />
+    </Route>
+    <Route path="register" element={<Register />} />
+    <Route path="login" element={<Login />} />
   </Route>
 )
 
