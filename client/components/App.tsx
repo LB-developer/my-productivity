51a7d54b428f740fa844f46e1baefba7dbc4d655
lblmdesk <loganbendev@gmail.com>
2024-10-26 23:11:05 +1300

    styling: ensure 100% width of outlet content
    - with the sidebar to the left on all pages in the outlet there was some
    strange behaviour in regard to trying to style widgets/components and
    their associated width properties. Ensuring 100% width ensures
    consistency and reduces unexplainable styling mishaps

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Sidebar from "./Sidebar"
 function App() {
+
+
   return (
     <div className="app-container d-flex flex-row">
       <Sidebar />
-      <div className="content-area">
+      <div className="content-area w-100">
         <Outlet />
       </div>
     </div>
