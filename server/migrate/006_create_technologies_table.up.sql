f78453595577b86adfa3fe82062ad83f6ac9de93
lblmdesk <loganbendev@gmail.com>
2024-10-28 15:49:31 +1300

    feat(schema): add polymorphic technologies table
    - Introduced `context_type` and `context_id` to link technologies to
    multiple entities.
    - Added `created_at` and `updated_at` timestamps for tracking changes.

--- server/migrate/006_create_technologies_table.up.sql
+++ server/migrate/006_create_technologies_table.up.sql
@@ -1,7 +1,10 @@
 CREATE TABLE technologies (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    project_id INTEGER,
-    name TEXT,
-    image_link TEXT,
-    FOREIGN KEY (project_id) REFERENCES projects(id)
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  context_id INTEGER NOT NULL, -- 'project.id' , 'course.id', etc..
+  context_type TEXT NOT NULL, -- 'project', 'course', etc...
+  name TEXT NOT NULL,
+  image_link TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
