39d3dcd8e46cb71e7a9b0aa44b539a9b8e498dd5
lblmdesk <loganbendev@gmail.com>
2024-10-28 16:13:06 +1300

    chore(db): update all_migrations script
    - Added new files(tables) i.e., `..._create_milestones` to run all
    migration files sequentially.
    - Automates the setup of new database structure.
    - Removed todos .read as table is no longer active.

--- server/migrate/all_migrations.up.sql
+++ server/migrate/all_migrations.up.sql
@@ -5,5 +5,9 @@
 .read ../migrate/004_create_tasks_table.up.sql
 .read ../migrate/005_create_projects_table.up.sql
 .read ../migrate/006_create_technologies_table.up.sql
-.read ../migrate/007_create_todos_table.up.sql
 .read ../migrate/008_create_notes_table.up.sql
+.read ../migrate/009_create_milestones_table.up.sql
+.read ../migrate/010_create_roadmaps_table.up.sql
+.read ../migrate/011_create_sessions_table.up.sql
+.read ../migrate/012_create_tags_table.up.sql
+.read ../migrate/013_create_tag_assignments_table.up.sql
