e3ad7890a374986dbfcc096c2a4ff902f90d5f48
lblmdesk <loganbendev@gmail.com>
2024-10-28 15:53:08 +1300

    refactor(schema): delete unused todos table
    - The table is no longer necessary as it is replaced by tasks

--- server/migrate/007_create_todos_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE todos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    project_id INTEGER,
-    description TEXT,
-    priority INTEGER,
-    FOREIGN KEY (project_id) REFERENCES projects(id)
-);
