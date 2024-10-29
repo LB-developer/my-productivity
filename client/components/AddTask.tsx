0823074b16b0f52a217597825d966daf37be85b8
lblmdesk <loganbendev@gmail.com>
2024-10-29 22:24:58 +1300

    feat: AddTask now uses context when creating a task
    - Reflecting changes in the db schema, the add task button takes context
    via prop drilling and passes it to a useMutation

--- client/components/AddTask.tsx
+++ client/components/AddTask.tsx
@@ -3,16 +3,32 @@ import { useCreateNewTask } from "../hooks/Tasks/Tasks"
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../src/store/ContextProvider";
 
-export default function AddTask() {
+interface Props {
+  contextType: string | null
+  contextId: number | null
+  parentTaskId: number | null
+}
+
+export default function AddTask({ contextType, contextId, parentTaskId }: Props) {
   const auth = useAuth();
-  if (!auth.user) return <p>Please log in.</p>
-  const mutate = useCreateNewTask(auth.user.publicId);
-  const navigate = useNavigate();
+  if (!auth || !auth.user?.publicId) return <p>Checking user data...</p>
+  const mutate = useCreateNewTask();
+  // const navigate = useNavigate();
 
   const handleCreateTask = async () => {
-    if (auth.user) {
-      const { taskId } = await mutate.mutateAsync(auth.user.publicId)
-      navigate(`/tasks/${taskId}`)
+    if (auth.user && auth.user.publicId) {
+      try {
+        const publicUserId = auth.user.publicId
+        const { taskId } = await mutate.mutateAsync({
+          publicUserId,
+          contextType,
+          contextId,
+          parentTaskId
+        })
+        // navigate(`/tasks/${taskId}`)
+      } catch (error) {
+        console.error("Failed to create task");
+      }
     }
   }
 
