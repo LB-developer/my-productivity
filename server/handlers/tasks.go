3f2a71901b549837ff89034eae9a9239bd97f4d2
lblmdesk <loganbendev@gmail.com>
2024-10-30 13:45:13 +1300

    feat: receive and pass context for new tasks
    - Our create new task handler gets the public id from the request url,
    the context values from the request body and passes them to the db
    function to be inserted into the db
    - With adding a request body an error check has been made to ensure the
    data has a max 1MB size and is only valid if the content type is
    application/json
    
    ALSO
    - userId -> publicUserId for accuracy/readability

--- server/handlers/tasks.go
+++ server/handlers/tasks.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	server "productivity/server/db"
 	"productivity/server/models"
@@ -51,7 +52,7 @@ func GetTodaysTasksHandler(w http.ResponseWriter, req *http.Request) {
 	}
 	defer db.Close()
 
-	todaysTasks, err := models.GetSchedulePreview(db, userPublicID)
+	todaysTasks, err := models.GetPriorityTasks(db, userPublicID)
 	if err != nil {
 		log.Printf("Couldn't query db %v", err)
 		http.Error(w, "Couldn't query prod.db", http.StatusInternalServerError)
@@ -63,7 +64,7 @@ func GetTodaysTasksHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func GetAllTasksHandler(w http.ResponseWriter, req *http.Request) {
-	userPublicID := req.URL.Query().Get("userId")
+	userPublicID := req.URL.Query().Get("publicUserId")
 	if userPublicID == "" {
 		http.Error(w, "Missing userId query", http.StatusBadRequest)
 		return
@@ -89,9 +90,34 @@ func GetAllTasksHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func CreateNewTaskHandler(w http.ResponseWriter, req *http.Request) {
-	userPublicID := req.URL.Query().Get("userId")
+	userPublicID := req.URL.Query().Get("publicUserId")
 	if userPublicID == "" {
-		http.Error(w, "Missing userId query", http.StatusBadRequest)
+		http.Error(w, "Missing userPublicId query", http.StatusBadRequest)
+		return
+	}
+
+	// The only valid media type is app/json
+	ct := req.Header.Get("Content-Type")
+	if ct != "" {
+		mediaType := strings.ToLower(strings.TrimSpace(strings.Split(ct, ";")[0]))
+		if mediaType != "application/json" {
+			http.Error(w, "Content-Type header is not application/json", http.StatusUnsupportedMediaType)
+			return
+		}
+	}
+
+	// Set maximum body size of 1MB
+	req.Body = http.MaxBytesReader(w, req.Body, 1048576)
+
+	// Should the field contain unknown fields the program will return with an error stating as much
+	dec := json.NewDecoder(req.Body)
+	dec.DisallowUnknownFields()
+
+	var defaultTask models.DefaultTask
+	err := dec.Decode(&defaultTask)
+	if err != nil {
+		log.Printf("Couldn't decode the request body of a new task \n%v", err)
+		http.Error(w, "Couldn't decode the request body of a new task", http.StatusInternalServerError)
 		return
 	}
 
@@ -104,7 +130,7 @@ func CreateNewTaskHandler(w http.ResponseWriter, req *http.Request) {
 	defer db.Close()
 
 	// pass off the creation to our db function
-	taskId, err := models.CreateNewTask(db, userPublicID)
+	taskId, err := models.CreateNewTask(db, userPublicID, defaultTask)
 	if err != nil {
 		fmt.Printf("Couldn't insert new task into db \n%v", err)
 		http.Error(w, "Couldn't insert new task into db \n%v", http.StatusInternalServerError)
