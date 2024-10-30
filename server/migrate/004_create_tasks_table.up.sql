01a7ad33af8b1382302f4abdc5b8ac3f856af6c0
lblmdesk <loganbendev@gmail.com>
2024-10-30 20:17:59 +1300

    refactor(schema): make context_type nullable
    - allows the user to create ad hoc tasks i.e., unrelated to a specified
    course, project etc...

--- server/migrate/004_create_tasks_table.up.sql
+++ server/migrate/004_create_tasks_table.up.sql
@@ -1,10 +1,9 @@
--- tasks_up.sql
 CREATE TABLE tasks (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   user_id INTEGER NOT NULL, -- Linked to users.id
   name TEXT NOT NULL,
   deadline DATE NOT NULL, -- Task deadline
-  context_type TEXT NOT NULL, -- E.g., 'course', 'project'
+  context_type TEXT, -- E.g., 'course', 'project'
   context_id INTEGER, -- ID of the associated entity or null when ad hoc
   priority INTEGER DEFAULT 0, -- Task priority (higher = more urgent)
   milestone_id INTEGER, -- Optional link to milestones.id
