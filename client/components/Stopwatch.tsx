c0653ac9240e646777f8ec41d8f8f6ae93ef5ada
lblmdesk <loganbendev@gmail.com>
2024-11-01 19:50:06 +1300

    refactor: remove unnecessary useEffect

--- client/components/Stopwatch.tsx
+++ client/components/Stopwatch.tsx
@@ -1,59 +1,18 @@
-import { useEffect } from "react"
 import { Button } from "react-bootstrap"
 
 interface Props {
-  studySessionTimeElapsed: Map<string, number>
-  setStudySessionTimeElapsed: React.Dispatch<
-    React.SetStateAction<Map<string, number>>
-  >
-  isRunning: boolean
+  timeElapsed: number
 }
 export default function Stopwatch({
-  studySessionTimeElapsed,
-  setStudySessionTimeElapsed,
-  isRunning,
+  timeElapsed,
 }: Props) {
-  /*
-    Our useEffect adds 1 second/minute/hour based on the current time elapsed
 
-    It works by incrementing time based on the current value  being less than 59
-    else when
-    60 seconds have passed - reset seconds to 0 and add one to minute
-    60 minutes have passed - reset minutes to 0 and add one to hour
-  */
-  useEffect(() => {
-    if (isRunning) {
-      const interval = setInterval(() => {
-        if ((studySessionTimeElapsed.get("seconds") ?? 0) < 59) {
-          setStudySessionTimeElapsed((prevTime) => {
-            const lastTime = new Map([...prevTime])
-            // current seconds are less than 59 e.g. XX:XX:58 therefore it is safe to increment
-            lastTime.set("seconds", (lastTime.get("seconds") ?? 0) + 1)
-
-            return lastTime
-          })
-        } else if ((studySessionTimeElapsed.get("minutes") ?? 0) < 59) {
-          setStudySessionTimeElapsed((prevTime) => {
-            const lastTime = new Map([...prevTime])
-            lastTime.set("minutes", (lastTime.get("minutes") ?? 0) + 1) // increment minutes passed by one
-            lastTime.set("seconds", 0) // 60 seconds have passed - reset seconds to zero
-
-            return lastTime
-          })
-        } else {
-          setStudySessionTimeElapsed((prevTime) => {
-            const lastTime = new Map([...prevTime])
-            lastTime.set("hours", (lastTime.get("hours") ?? 0) + 1) // increment hours passed by one
-            lastTime.set("minutes", 0) // 60 minutes have passed - reset minutes to zero
-            lastTime.set("seconds", 0) // 60 seconds have passed - reset seconds to zero
-
-            return lastTime
-          })
-        }
-      }, 500) // TODO: when stopwatch is complete, change to 1000 for accurate counting
-      return () => clearInterval(interval)
-    }
-  }, [isRunning, studySessionTimeElapsed, setStudySessionTimeElapsed])
+  const sec = timeElapsed % 60
+  const min = Math.floor((timeElapsed % 3600) / 60)
+  const hour = Math.floor(timeElapsed / 3600)
+  const seconds = String(sec).padStart(2, "0")
+  const minutes = String(min).padStart(2, "0")
+  const hours = String(hour).padStart(2, "0")
 
   return (
     <Button
@@ -61,19 +20,7 @@ export default function Stopwatch({
       className="d-flex flex-row justify-content-center h-100"
       size="lg"
     >
-      {Array.from(studySessionTimeElapsed.entries()).map(
-        ([time, value], index) => {
-          return (
-            <div key={time}>
-              <span>
-                {/* print the stopwatch time as HH:MM:SS */}
-                {String(value).padStart(2, "0")}
-                {index < 2 && ":"}
-              </span>
-            </div>
-          )
-        }
-      )}
+      {`${hours}:${minutes}:${seconds}`}
     </Button>
   )
 }
--- client/components/Widgets/NewStudySession.tsx
+++ client/components/Widgets/NewStudySession.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faFloppyDisk,
@@ -8,69 +8,58 @@ import {
 import { Button } from "react-bootstrap"
 import Stopwatch from "../Stopwatch"
 
-const startingTime: Map<string, number> = new Map([
-  ["hours", 0],
-  ["minutes", 0],
-  ["seconds", 0],
-])
 
 export default function NewStudySession() {
-  const [studySessionTimeElapsed, setStudySessionTimeElapsed] =
-    useState<Map<string, number>>(startingTime)
+  const [timeElapsed, setTimeElapsed] = useState<number>(0)
   const [isRunning, setIsRunning] = useState<boolean>(false)
+  let intervalRef = useRef<ReturnType<typeof setInterval>>()
 
-  const handleResetSession = () => {
-    setStudySessionTimeElapsed(startingTime)
+  const handleStart = () => {
+    if (isRunning) return
+    setIsRunning(true)
+    intervalRef.current = setInterval(() => {
+      setTimeElapsed((curr) => curr + 1)
+    }, 1000)
+  }
+
+  const handleStop = () => {
+    if (!isRunning) return
+    setIsRunning(false)
+    clearInterval(intervalRef.current)
+  }
+
+  const handleReset = () => {
+    setTimeElapsed(0)
     setIsRunning(false)
   }
 
   return (
     <div className="d-flex flex-column gap-1 p-3">
-      <h4>New Study Session</h4>
+      <h4>New Focus Session</h4>
       <Stopwatch
-        studySessionTimeElapsed={studySessionTimeElapsed}
-        setStudySessionTimeElapsed={setStudySessionTimeElapsed}
-        isRunning={isRunning}
+        timeElapsed={timeElapsed}
       />
       {isRunning ? (
         <div className="d-flex flex-row justify-content-center gap-1">
           <Button
             variant="outline-primary"
             className="flex-grow-1"
-            onClick={() => setIsRunning(false)}
+            onClick={handleStop}
           >
             <FontAwesomeIcon icon={faPause} />
           </Button>
-          {/* TODO: Send user to task viewer */}
-          <Button onClick={() => setIsRunning(false)}>
-            Open in task viewer
+          {/* TODO: Send user to session viewer */}
+          <Button >
+            Open in session viewer
           </Button>
         </div>
       ) : (
         <div className="d-flex flex-row justify-content-center gap-1">
-          {/* 
-              If a session has been started and not 'stopped' (different from pause), 
-              studySessionTimeElapsed will have a current value greater than one which we use
-              to render the continue button.
-              
-              Should a user 'stop' the timer or save the current session as a task, 
-              a task/session will be created with the current time as a value;
-              all other values being default.
-          */}
-          {Math.max(...Array.from(studySessionTimeElapsed.values())) > 0 ? (
-            <Button
-              variant="outline-primary"
-              className="w-100"
-              onClick={() => setIsRunning(true)}
-            >
-              <FontAwesomeIcon icon={faPlay} />
-            </Button>
-          ) : (
+          {/* The clock is not running */}
+          {(
             <Button
               className=""
-              onClick={() => (
-                setIsRunning(true), setStudySessionTimeElapsed(startingTime)
-              )}
+              onClick={handleStart}
             >
               <FontAwesomeIcon icon={faPlay} />
             </Button>
@@ -78,7 +67,7 @@ export default function NewStudySession() {
           <Button
             variant="outline-primary"
             className=""
-            onClick={handleResetSession}
+            onClick={handleReset}
           >
             Reset
           </Button>
