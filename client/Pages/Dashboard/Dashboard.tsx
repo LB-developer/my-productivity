e3e8a15c2b757c1c29d223ae1f26b19ad0675265
lblmdesk <loganbendev@gmail.com>
2024-11-05 21:23:23 +1300

    refactor/styling: userId becomes publicUserId,
    switches to dark mode colours by default

--- client/Pages/Dashboard/Dashboard.tsx
+++ client/Pages/Dashboard/Dashboard.tsx
@@ -6,6 +6,7 @@ import MonthlySpend from "../../components/Widgets/MonthlySpend"
 import TodaysTasks from "../../components/Widgets/TodaysTasks"
 import NewStudySession from "../../components/Widgets/NewStudySession"
 import UserOverview from "../../components/Widgets/UserOverview"
+import Stash from "../../components/Widgets/Stash"
 import SearchBar from "../../components/SearchBar"
 import { Container, Row, Col } from "react-bootstrap"
 import { Helmet } from "react-helmet"
@@ -14,10 +15,10 @@ import { useAuth } from "../../src/store/ContextProvider"
 export default function Dashboard() {
   const auth = useAuth();
   if (!auth || !auth.user?.publicId) return <p>Checking user data...</p>
-  const userId = auth.user?.publicId
+  const publicUserId = auth.user?.publicId
 
   return (
-    <div className="bg-secondary bg-opacity-25 ">
+    <div className="mt-3 ms-3">
       <Helmet>
         <title>Dashboard</title>
       </Helmet>
@@ -26,46 +27,53 @@ export default function Dashboard() {
           <Row className="gap-3 gy-2">
             {/* Header Section */}
             <Col xs={7} className="d-flex flex-column justify-content-center">
-              <p className="mb-0 mt-4">Welcome to your,</p>
-              <h2 className="fw-bold display-6">Productivity Dashboard</h2>
+              <p className="mb-0">Welcome to your,</p>
+              <h2 className="fw-bold display-6 mb-0">Dashboard</h2>
             </Col>
-            <Col xs={4} className=" ms-auto mt-auto bg-light rounded-4">
+            <Col xs={4} className=" ms-auto mt-auto bg-dark  rounded-4">
               <SearchBar />
             </Col>
           </Row>
           <Row className="gap-3 gy-2">
-            <Col className="bg-light ms-3 rounded-4">
-              <MonthlyHours userId={userId} />
+            <Col xs={3} className="bg-primary-subtle rounded-4">
+              <MonthlyHours publicUserId={publicUserId} />
             </Col>
 
-            <Col className="bg-danger bg-opacity-50">
-              <MonthlySpend />
+            <Col xs={3} className="bg-primary-subtle rounded-4">
+              <MonthlyHours publicUserId={publicUserId} />
             </Col>
 
-            <Col className="bg-light rounded-4">
+            <Col xs={2} className="bg-primary-subtle">
+              <Col className="bg-danger h-50">
+                <MonthlySpend />
+              </Col>
+              <Col className="bg-primary h-50">
+                <Stash />
+              </Col>
+            </Col>
+            <Col className="bg-primary-subtle rounded-4">
               <NewStudySession />
             </Col>
           </Row>
           <Row className="gap-3 gy-2">
-            <Col className="bg-light ms-3 rounded-4">
-              <TodaysTasks userId={userId} />
+            <Col xs={8} className="bg-primary-subtle rounded-4">
+              <TodaysTasks publicUserId={publicUserId} />
             </Col>
 
-            <Col className="bg-light rounded-4">
-              <CoursesPreview userId={userId} />
-            </Col>
-
-            <Col className="bg-warning bg-opacity-50">
-              <ActivityPreview />
+            <Col className="bg-primary-subtle rounded-4">
+              <CoursesPreview publicUserId={publicUserId} />
             </Col>
           </Row>
           <Row className="gap-3 gy-2 ">
-            <Col xs={6} className="bg-primary ms-3 bg-opacity-50 h-100">
+            <Col xs={4} className="bg-primary-subtle h-100">
               <CurrentProgress />
             </Col>
 
-            <Col className="bg-warning bg-opacity-75">
-              <UserOverview userId={userId} />
+            <Col xs={3} className="bg-primary-subtle">
+              <UserOverview publicUserId={publicUserId} />
+            </Col>
+            <Col className="bg-primary-subtle bg-opacity-50">
+              <ActivityPreview />
             </Col>
           </Row>
         </Row>
